9acb1a77facd6cddeb945f073d0ecadde200b288
havazelet <havazeletshlomo@gmail.com>
2025-05-16 01:09:12 +0300

    add stat card and fix side nav bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,21 @@
 import './App.css';
 import SideNavBar from './components/SideNavBar/SideNavBar';
 import Dashboard from './pages/Dashboard/Dashboard';
-import { Box } from '@mui/material';
+import { Box, CssBaseline } from '@mui/material';
 
 function App() {
   return (
     <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
       <SideNavBar />
-      <Box sx={{ flexGrow: 1, padding: 2 }}>
+      <Box 
+        component="main" 
+        sx={{ 
+          flexGrow: 1, 
+          padding: { xs: 1, sm: 2 },
+          marginLeft: { xs: 0, sm: 0 }
+        }}
+      >
         <Dashboard />
       </Box>
     </Box>
--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,20 @@
+import { Grid } from "@mui/material";
+import StatCard from "../../components/Card/StatCard"; 
+import statData from "../../mocks/statData";
+import styles from "./Dashboard.module.css";
+
 const Dashboard = () => {
-    return (<div>
-        dashboard
-    </div>)
-} 
+  return (
+    <div className={styles.container}>
+      <Grid container spacing={2}>
+        {statData.map((item) => (
+          <Grid key={item.title} className={styles.gridItem}>
+            <StatCard {...item} />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+};
 
 export default Dashboard;
